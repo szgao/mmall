958d8ef055ba1678965995527a8e1ce66c5fed28
szgao <564763922@qq.com>
2018-02-09 13:20:24 +0800

    逆向工程生成Mapper.xml和dao层接口

---
+++ src/main/java/com/gaochao/mmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.gaochao.mmall.dao;
+
+import com.gaochao.mmall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
